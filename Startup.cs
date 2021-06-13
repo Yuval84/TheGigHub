1c803a5911aa80a6d4a62d6f6a55743162d61dbf
Yuval Idelman <yuvale84@gmail.com>
2021-06-13 15:32:04 +0300

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TheGigHub.Startup))]
+namespace TheGigHub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
