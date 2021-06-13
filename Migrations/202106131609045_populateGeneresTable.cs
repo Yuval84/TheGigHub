2b60ca0990d3901a3ee578568236230e06dc1281
Yuval Idelman <yuvale84@gmail.com>
2021-06-13 19:29:03 +0300

    Populate Genres Table

---
+++ Migrations/202106131609045_populateGeneresTable.cs
@@ -0,0 +1,23 @@
+namespace TheGigHub.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class populateGeneresTable : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO Genres (Name) VALUES ('Jazz')");
+            Sql("INSERT INTO Genres (Name) VALUES ('Rock')");
+            Sql("INSERT INTO Genres (Name) VALUES ('Metal')");
+            Sql("INSERT INTO Genres (Name) VALUES ('Classic')");
+            Sql("INSERT INTO Genres (Name) VALUES ('HipHop')");
+            Sql("INSERT INTO Genres (Name) VALUES ('Bluse')");
+        }
+        
+        public override void Down()
+        {
+            Sql("DELETE FROM Genres WHERE Id IN (1, 2, 3, 4, 5, 6)");
+        }
+    }
+}
