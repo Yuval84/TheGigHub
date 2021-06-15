8d8fe9eb9573e9d4b8d73ccbd1ff2a5534224c31
Yuval Idelman <yuvale84@gmail.com>
2021-06-15 11:44:46 +0300

    Design Changes

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -24,7 +24,8 @@ namespace TheGigHub
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/override.css" ));
         }
     }
 }
